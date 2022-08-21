4e5584b30cd5d3304571c716e156b15dd2c0e509
Jordan Gantier R <jordangantier@gmail.com>
2022-08-21 13:03:06 -0400

    Versión 1.1.2 - Agregado Datos Impresión all en la api.

--- app/Http/Controllers/DatosImpresionController.php
+++ app/Http/Controllers/DatosImpresionController.php
@@ -7,6 +7,24 @@ use App\Models\Transaccion;
 class DatosImpresionController extends Controller
 {
     /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $toPrint = Transaccion::select('transaccions.id as id_transaccion', 'users.name as usuario', 'nombre', 'ci_nit', 'telefono', 'participantes.email', 'transaccions.created_at as fecha', 'monto_acumulado', 'qty_boletos', 'habilitados')
+            ->join('participantes', 'participantes.id', '=', 'transaccions.id_participante')
+            ->join('users', 'users.id', '=', 'transaccions.id_user')
+            ->orderBy('id_transaccion', 'DESC')
+            ->get();
+
+        if ($toPrint == '[]') {
+            $toPrint = json_decode('[{"msg":"No hay datos para imprimir."}]');
+        }
+        return $toPrint;
+    }
+    /**
      * Display the specified resource.
      *
      * @param  int  $id
