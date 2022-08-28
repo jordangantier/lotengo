5e83cdc47a0758a2d63132a625abf74dc4200909
Jordan Gantier R <jordangantier@gmail.com>
2022-08-28 14:44:10 -0400

    Mejorada la seguridad del cifrador de hashes.

--- database/seeders/BoletoSeeder.php
+++ database/seeders/BoletoSeeder.php
@@ -10,24 +10,28 @@ class BoletoSeeder extends Seeder
 {
     public function run()
     {
-        //Marcas generadas
+        $cantidad_numeros = 93;
+        $cantidad_boletos = 15000;
+        $cantidad_jugadas = 3;
+        $cantidad_series = 6;
+
+        $modulo = $cantidad_boletos % $cantidad_series;
+        $boletos_x_serie = ($cantidad_boletos - $modulo) / $cantidad_series;
+        $emitibles = $boletos_x_serie * $cantidad_series;
+
+        $ciphering = env('CIPHERING');
+        $iv_length = openssl_cipher_iv_length($ciphering);
+        $encryption_iv = env('ENCRYPTION_IV');
+        $encryption_key = env('ENCRYPTION_KEY');
+
+        //Genera el array de números sorteables.
         $boletos = array();
-        for ($i = 1; $i <= 93; $i++) {
+        for ($i = 1; $i <= $cantidad_numeros; $i++) {
             $boletos[$i] = $i;
         }
 
-        // Storingthe cipher method 
-        $ciphering = "AES-128-CTR";
-        // Using OpenSSl Encryption method 
-        $iv_length = openssl_cipher_iv_length($ciphering);
-        $options   = 0;
-        // Non-NULL Initialization Vector for encryption 
-        $encryption_iv = '7076405498371295';
-        // Storing the encryption key 
-        $encryption_key = "WHEKE.DEV@Las Brisas";
-
-        //Genera los 10000 boletos
-        for ($i = 1; $i <= 15000; $i++) {
+        //Genera los boletos
+        for ($i = 1; $i <= $cantidad_boletos; $i++) {
 
             //Coloca los boletos en su determinado concurso.
             if ($i >= 1 && $i <= 2500) {
@@ -50,16 +54,17 @@ class BoletoSeeder extends Seeder
             }
 
             //Crea los tres arrays de boletos.
-            for ($n = 1; $n <= 3; $n++) {
+            for ($n = 1; $n <= $cantidad_jugadas; $n++) {
                 $numeros[$n] = array_rand($boletos, 15);
             }
 
             //Genera el json con todos los números.
-            $array_numeros = json_encode(array($numeros[1], $numeros[2], $numeros[3]));
+            $array_numeros = json_encode(array('1' => $numeros[1], '2' => $numeros[2], '3' => $numeros[3]));
 
             //Generación de Hash.
             $hasher = $concurso . '|' . $i . '|' . $numeros[1][0] . '|' . $numeros[2][0] . '|' . $numeros[3][0];
-            $hash = openssl_encrypt($hasher, $ciphering, $encryption_key, $options, $encryption_iv);
+            $hash = openssl_encrypt($hasher, $ciphering, $encryption_key, 0, $encryption_iv);
+            //$decrypted = openssl_decrypt($hash, $ciphering, $encryption_key, 0, $encryption_iv);
 
             /*
             Boleto::create([
